9e835e2655d8f5a6f31f0757732e67ceffa3dbc5
Vilmar Prestes <prestesvilmar20@gmail.com>
2024-08-21 23:49:46 -0300

    feat: adição de arquivo para configurar no SceneBuilder e adequação da main class para o mesmo.

--- src/main/java/com/javafx_voltech_cps/cameramonitoringapp/Main.java
+++ src/main/java/com/javafx_voltech_cps/cameramonitoringapp/Main.java
@@ -1,2 +1,26 @@
-package com.javafx_voltech_cps.cameramonitoringapp;public class Main {
+package com.javafx_voltech_cps.cameramonitoringapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main-view.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("Camera Switching App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
