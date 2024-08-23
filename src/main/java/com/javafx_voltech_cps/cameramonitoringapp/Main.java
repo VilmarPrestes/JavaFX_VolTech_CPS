d2c2de399ecd92c622692369ab9fdb75d34d0a57
Vilmar Prestes <prestesvilmar20@gmail.com>
2024-08-23 19:49:37 -0300

    feat: inserções de commits do Eduardo.

--- src/main/java/com/javafx_voltech_cps/cameramonitoringapp/Main.java
+++ src/main/java/com/javafx_voltech_cps/cameramonitoringapp/Main.java
@@ -5,22 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
-
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Camera Switching App");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+	
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,14 @@
-module com.javafx_voltech_cps.cameramonitoringapp {
+	module com.javafx_voltech_cps.cameramonitoringapp {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.desktop;
+	requires opencv;
 
 
     opens com.javafx_voltech_cps.cameramonitoringapp to javafx.fxml;
     exports com.javafx_voltech_cps.cameramonitoringapp;
     exports com.javafx_voltech_cps.cameramonitoringapp.controller;
     opens com.javafx_voltech_cps.cameramonitoringapp.controller to javafx.fxml;
-    exports com.javafx_voltech_cps.cameramonitoringapp.model;
-    opens com.javafx_voltech_cps.cameramonitoringapp.model to javafx.fxml;
+    exports com.javafx_voltech_cps.cameramonitoringapp.model.entity;
+    opens com.javafx_voltech_cps.cameramonitoringapp.model.entity to javafx.fxml;
 }
