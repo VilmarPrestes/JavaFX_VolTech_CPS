579a79755224f4bf1a0c4e181481532bf44b4ddc
EduardoCaixeta <eduardomart1934@gmail.com>
2024-08-28 20:18:53 -0300

    Començando painel de cadastro

---
+++ src/main/java/com/javafx_voltech_cps/cameramonitoringapp/view/forms/FormAddRecorder.java
@@ -0,0 +1,9 @@
+package com.javafx_voltech_cps.cameramonitoringapp.view.forms;
+
+import com.javafx_voltech_cps.cameramonitoringapp.view.panels.PanelAddRecorder;
+
+public class FormAddRecorder {
+
+    private PanelAddRecorder panel;
+
+}
---
+++ src/main/java/com/javafx_voltech_cps/cameramonitoringapp/view/panels/PanelAddRecorder.java
@@ -0,0 +1,17 @@
+package com.javafx_voltech_cps.cameramonitoringapp.view.panels;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PanelAddRecorder {
+
+    @FXML
+    private TextField txtName;
+
+
+    @FXML
+    public void initialize(){
+
+    }
+
+}
